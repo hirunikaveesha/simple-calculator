2527a32ee0030ace4f9a12a872788c34dd8ae86e
hirunikaveesha <88107879+hirunikaveesha@users.noreply.github.com>
2022-05-17 12:44:34 +0530

    Add files via upload

---
+++ calculator/calculator1.py
@@ -0,0 +1,38 @@
+while True:
+    print("1 Addition")
+    print("2 Subtraction")
+    print("3 Multification")
+    print("4 Division")
+    print("Enter q or Q to exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "q" or choice == "Q":
+        break
+   
+
+    num1=float(input("Enter number 1: "))
+    num2=float(input("Enter number 2: "))
+
+    if choice == "1":
+        print(num1, "+", num2, "=",(num1+num2))
+    elif choice=="2":
+        print(num1, "-", num2, "=",(num1-num2))
+    elif choice=="3":
+        print(num1, "*", num2, "=",(num1*num2))  
+    elif choice =="4":
+        if num2==0.0:
+            print("Divide by 0 Error")
+        else:
+            print(num1, "/", num2, "=",(num1/num2))
+    else:
+         print("invalid choice")
+
+    print()
+
+
+  
+     
+       
+
+
